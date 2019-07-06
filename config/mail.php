346ef17b3e85e3f9a94b46688a8f3133bbf571ed
pufesz <1berecz.marton@gmail.com>
2019-07-06 21:50:45 +0100

    email again

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'jflfjoha.loginssl.com'),
+    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -71,7 +71,7 @@ return [
     |
     */
 
-    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+    'encryption' => env('MAIL_ENCRYPTION', 'TLS'),
 
     /*
     |--------------------------------------------------------------------------
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('info@zsindelyestetofedes.hu'),
+    'username' => env('1berecz.marton@gmail.com'),
 
-    'password' => env('Hoember2255'),
+    'password' => env('*$t4r*P0rt*'),
 
     /*
     |--------------------------------------------------------------------------
